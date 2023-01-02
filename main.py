f1e3a0eb151740e97e25a254d78699716d4c74bd
Pawel Pawluk <p2well@outlook.com>
2023-01-02 10:54:26 +0000

    Rewrite Shell in Python

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Python based GitHub Action
+"""
+import os
+import time
+
+import requests  # pylint: disable=unused-import # noqa We are just importing this to prove the dependency installed correctly
+
+
+def main():
+    """Greets someone and outputs current time
+    """
+    who_to_greet = os.environ["INPUT_WHO-TO-GREET"]
+    current_time = time.ctime()
+
+    print(f'Hello, {who_to_greet}')
+
+    print(f'::set-output name=time::{current_time}')
+
+
+if __name__ == '__main__':
+    main()
